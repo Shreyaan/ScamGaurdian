ef5b4657feca5b57db733117012b259cb563e244
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-04 23:11:45 +0530

    double click hides it

--- content.js
+++ content.js
@@ -166,7 +166,32 @@ document.body.appendChild(popup);
 // fix
 popup.style.display = "none";
 
-// button.addEventListener("click", function () {});
+let isLongPressing = false;
+let buttonMoved = false; // track if the button was moved during long press
+let lastClickTime = 0; // track the last click time
+let buttonMovedState = false; // track if the button is currently moved
+
+button.addEventListener("click", function (event) {
+  const currentTime = new Date().getTime();
+  if (currentTime - lastClickTime <= 300) {
+    // Double click
+    if (buttonMovedState) {
+      button.style.right = "10px";
+
+      buttonMovedState = false;
+    } else {
+      // Move the button to the right
+      button.style.right = "-50px";
+      buttonMovedState = true;
+    }
+    button.style.transition = "right 0.5s";
+    setTimeout(function () {
+      button.style.transition = ""; // Reset transition
+    }, 500);
+  }
+  lastClickTime = currentTime;
+  event.preventDefault(); // Prevent default behavior of double-click
+});
 
 button.addEventListener("mousedown", function () {
   isLongPressing = true;
@@ -175,7 +200,7 @@ button.addEventListener("mousedown", function () {
     if (isLongPressing) {
       document.addEventListener("mousemove", followMouse);
     }
-  }, 500); // Adjust the delay as needed
+  }, 500);
 });
 
 document.addEventListener("mouseup", function () {
@@ -184,14 +209,18 @@ document.addEventListener("mouseup", function () {
     document.removeEventListener("mousemove", followMouse);
 
     if (!buttonMoved) {
-      if (popup.style.display === "none") {
-        popup.style.display = "block";
-      } else {
-        popup.style.display = "none";
-      }
+      togglePopup();
     }
   }
 });
+
+function togglePopup() {
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
 
 function followMouse(event) {
   const mouseY = event.clientY;
