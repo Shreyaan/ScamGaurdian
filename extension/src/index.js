672ed80df3bd86251b5153f8d2dcc36861107c2a
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-05 21:20:03 +0530

    webpack added

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,12 +1,11 @@
-// Create the button
+import { cssStyles } from "./cssStyles.js";
+
 let button = document.createElement("button");
 button.innerHTML = "SG";
 
 button.id = "myExtensionButton";
 // Create the popup (initially hidden)
 let popup = document.createElement("div");
-
-
 
 popup.id = "myExtensionPopup";
 
@@ -50,104 +49,6 @@ function addButtonToWebsites() {
 }
 
 addButtonToWebsites();
-
-const cssStyles = `
-#myExtensionButton {
-  position:fixed;
-  right:10px;
-  bottom:200px;
-  z-index:999;
-  padding:1rem;
-  font-size:1.5rem;
-  background-color:#0F69D2;
-  color:#fff;
-  border:none;
-  font-weight:700;
-  transition: transform ease-in-out .3s;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-  border-radius:10px;
-  cursor:pointer;
-}
-#myExtensionButton:hover{
-  transform:scale(1.2);
-}
-#myExtensionPopup{
-  width:25vw;
-  height:auto;
-  max-height: 80vh;
-  right:4rem;
-  top:1rem;
-  border:1px solid #000;
-  border-radius : 5px;
-  position:fixed;
-  z-index:10000;
-  background-color:#fff;
-  display:none;
-  overflow-y: scroll;
-}
-#myExtensionPopup .header{
-  background:#0F69D2;
-   width:25vw;
-  color:white;
-  text-align:center;
-  font-weight:700;
-  font-size:1em;
-  padding:8px;
-  cursor:pointer;
-}
-#myExtensionPopup section{
-  position:relative;
-  overflow:hidden;
-  color:black;
-  background-color:white;
-}
-
-#myExtensionPopup .listItem{
-  position:relative;
-  padding:0.5rem;
-  margin-right:0.5rem;
-
-}
-#myExtensionPopup p{
-  font-size:1rem;
-  font-weight:700;
-  margin:10px;
-}
-
-#myExtensionPopup .riskList{
-  font-size:1rem;
-  list-style-type:" ☠️ ";
-  padding-left:0.5rem;
-
-}
-
-#myExtensionPopup .tipsList{
-  font-size:1rem;
-  list-style-type:" 💡 ";
-  padding-left:0.5rem;
-}
-
-
-#myExtensionPopup .tipsText{
-  font-size:1.5rem;
-  color:green;
-  font-weight:600;
-  text-decoration:underline;
-}
-
-#myExtensionPopup .riskText{
-  font-size:1.5rem;
-  color:red;
-  font-weight:600;
-  text-decoration:underline;
-}
-
-#myExtensionPopup .border{
-  border:0.5px solid lightgray;
-  width:100%;
-}
-
-`;
 
 function injectCSS(cssStyles) {
   const styleElement = document.createElement("style");
@@ -223,7 +124,6 @@ function followMouse(event) {
   button.style.bottom = `${window.innerHeight - mouseY}px`;
   buttonMoved = true; // Set the flag to true when the button is moved
 }
-
 
 function generatePopUpHTML(data) {
   return `
---
+++ extension/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production', // Use 'development' for readable output
+    entry: './src/index.js', // Your main JS file
+    output: {
+        filename: 'content.js', // The bundled output file
+        path: path.resolve(__dirname, 'dist'), // Output folder
+    },
+    resolve: {
+        extensions: ['.js'], // File types to bundle together
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+    },
+    performance: {
+        hints: false, // Turns off size warnings for entry points
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Use babel-loader for JS files
+                exclude: /node_modules/, // Exclude node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'] // Use babel preset to ensure modern JS works
+                    }
+                }
+            }
+        ]
+    }
+};
