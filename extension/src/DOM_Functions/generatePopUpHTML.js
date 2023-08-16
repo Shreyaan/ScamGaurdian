7a4109dda6986569da77aaa68382fa74f8159d4a
Krunal Gupta <guptakrunal02@gmail.com>
2023-08-16 20:25:52 +0530

    design

--- extension/src/DOM_Functions/generatePopUpHTML.js
+++ extension/src/DOM_Functions/generatePopUpHTML.js
@@ -1,31 +1,77 @@
 export function generatePopUpHTML(data) {
   return `
-  <div>
-  <div class='header'>
-    <h1>Scam Guardian</h1>
-  </div>
+    <div>
+      <div class='header'>
+        <h1>Scam Guardian</h1>
+      </div>
+  
+      <section>
+  
+        <p class="riskText">Risks:</p>
 
-  <section>
+        <div class="accordion">
 
-  <p class="riskText">Risks</p>
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-1">
+             <label for="panel-1"> ☠️ Risk 1</label>
 
-  <ul class='listItem'>
-   <li class='list riskList'>${data.risks[0]}</li>
-   <li class='list riskList'>${data.risks[1]}</li>
-   <li class='list riskList'>${data.risks[2]}</li>
-  </ul>
+              <div class="accordion-content   accordion-content-small">
+                  <p class="accordion-body">${data.risks[0]}</p>
+                </div>
+          </div>
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-2">
+             <label for="panel-2"> ☠️ Risk 2</label>
 
-  <div class='border'></div>
+              <div class="accordion-content accordion-content-small">
+                  <p class="accordion-body">${data.risks[1]}</p>
+                </div>
+          </div>
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-3">
+             <label for="panel-3"> ☠️ Risk 3</label>
 
-  <p class="tipsText">Tips</p>
+              <div class="accordion-content   accordion-content-small">
+                  <p class="accordion-body">${data.risks[2]}</p>
+                </div>
+          </div>
+        </div>
 
-  <ul class="listItem">
-    <li class='list tipsList'>${data.tips[0]}</li>
-    <li class='list tipsList'>${data.tips[1]}</li>
-    <li class='list tipsList'>${data.tips[2]}</li>
-  </ul>
 
-  </section>
-  </div>
-`;
+        <p class="tipsText">Tips:</p>
+
+        <div class="accordion">
+
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-4">
+             <label for="panel-4"> 💡 Tip 1</label>
+
+              <div class="accordion-content   accordion-content-small">
+                  <p class="accordion-body">${data.tips[0]}</p>
+                </div>
+          </div>
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-5">
+             <label for="panel-5"> 💡 Tip 2</label>
+
+              <div class="accordion-content accordion-content-small">
+                  <p class="accordion-body">${data.tips[1]}</p>
+                </div>
+          </div>
+          <div class="panels">
+              <input type="checkbox" name="panel" id="panel-6">
+             <label for="panel-6"> 💡 Tip 3</label>
+
+              <div class="accordion-content   accordion-content-small">
+                  <p class="accordion-body">${data.tips[2]}</p>
+                </div>
+          </div>
+        </div>
+
+
+      </section>
+    </div>
+    
+    
+  `;
 }
