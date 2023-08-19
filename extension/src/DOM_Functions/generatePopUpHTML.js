e9cc8859e172c2db959804dabfa1f531117643fa
Krunal Gupta <guptakrunal02@gmail.com>
2023-08-19 19:37:46 +0530

    typo changes

--- extension/src/DOM_Functions/generatePopUpHTML.js
+++ extension/src/DOM_Functions/generatePopUpHTML.js
@@ -9,30 +9,30 @@ export function generatePopUpHTML(data) {
   
         <p class="riskText">Risks:</p>
 
-        <div class="accordion">
+        <div class="scamAccordian">
 
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-1">
              <label for="panel-1"> ☠️ Risk 1</label>
 
-              <div class="accordion-content   accordion-content-small">
-                  <p class="accordion-body">${data.risks[0]}</p>
+              <div class="scamAccordian-content   scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.risks[0]}</p>
                 </div>
           </div>
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-2">
              <label for="panel-2"> ☠️ Risk 2</label>
 
-              <div class="accordion-content accordion-content-small">
-                  <p class="accordion-body">${data.risks[1]}</p>
+              <div class="scamAccordian-content scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.risks[1]}</p>
                 </div>
           </div>
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-3">
              <label for="panel-3"> ☠️ Risk 3</label>
 
-              <div class="accordion-content   accordion-content-small">
-                  <p class="accordion-body">${data.risks[2]}</p>
+              <div class="scamAccordian-content   scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.risks[2]}</p>
                 </div>
           </div>
         </div>
@@ -40,30 +40,30 @@ export function generatePopUpHTML(data) {
 
         <p class="tipsText">Tips:</p>
 
-        <div class="accordion">
+        <div class="scamAccordian">
 
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-4">
              <label for="panel-4"> 💡 Tip 1</label>
 
-              <div class="accordion-content   accordion-content-small">
-                  <p class="accordion-body">${data.tips[0]}</p>
+              <div class="scamAccordian-content   scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.tips[0]}</p>
                 </div>
           </div>
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-5">
              <label for="panel-5"> 💡 Tip 2</label>
 
-              <div class="accordion-content accordion-content-small">
-                  <p class="accordion-body">${data.tips[1]}</p>
+              <div class="scamAccordian-content scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.tips[1]}</p>
                 </div>
           </div>
           <div class="panels">
               <input type="checkbox" name="panel" id="panel-6">
              <label for="panel-6"> 💡 Tip 3</label>
 
-              <div class="accordion-content   accordion-content-small">
-                  <p class="accordion-body">${data.tips[2]}</p>
+              <div class="scamAccordian-content   scamAccordian-content-small">
+                  <p class="scamAccordian-body">${data.tips[2]}</p>
                 </div>
           </div>
         </div>
