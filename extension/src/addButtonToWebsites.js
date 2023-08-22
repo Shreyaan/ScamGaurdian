a74ec30913a9c0a864384be55d173a99db966865
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-23 00:35:48 +0530

    check current domain

--- extension/src/addButtonToWebsites.js
+++ extension/src/addButtonToWebsites.js
@@ -3,26 +3,27 @@ import { getVendorInfo } from "./helper/getVendorInfo.js";
 import { createButton, createPopup } from "./DOM_Functions/createElements.js";
 import { appendElementsToBody } from "./helper/appendElementsToBody.js";
 import { setButtonBehaviour } from "./DOM_Functions/setButtonBehaviour.js";
+import { getWebsiteStatus } from "./helper/getWebsiteStatus.js";
 
 export let button = createButton();
 export let popup = createPopup();
 
-export function addButtonToWebsites() {
+export async function addButtonToWebsites() {
   let currentUrl = window.location.href;
-  let vendorInfo = {};
 
-  getVendorInfo(currentUrl)
-    .then((data) => {
-      vendorInfo = data;
-      if (data) {
-        if (data?.risks?.length > 0 || data?.tips?.length > 0) {
-          appendElementsToBody(button, popup);
-          setButtonBehaviour();
-          popup.innerHTML = generatePopUpHTML(vendorInfo);
-        }
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  let hostname = new URL(currentUrl).hostname;
+
+  let status = await getWebsiteStatus(hostname);
+  if (status) {
+    alert("This website is blacklisted");
+  }
+
+  let vendorData = await getVendorInfo(currentUrl);
+  if (vendorData) {
+    if (vendorData?.risks?.length > 0 || vendorData?.tips?.length > 0) {
+      appendElementsToBody(button, popup);
+      setButtonBehaviour();
+      popup.innerHTML = generatePopUpHTML(vendorData);
+    }
+  }
 }
