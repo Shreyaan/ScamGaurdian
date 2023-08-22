60b65c2c82cd4f923a7b679302064d115fb16f05
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-23 00:50:48 +0530

    fix

--- web/src/pages/api/check-domainsWhitelist.ts
+++ web/src/pages/api/check-domainsWhitelist.ts
@@ -2,7 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from "next";
 import { Pool, QueryResult } from "pg";
-import 'dotenv/config'
+import "dotenv/config";
 
 let password = process.env.passwordvendor;
 
@@ -32,7 +32,9 @@ export default async function handler(
     const client = await pool.connect();
 
     // Generate a string with placeholders for the domains
-    const domainPlaceholders = domains.map((_, index) => `$${index + 1}`).join(",");
+    const domainPlaceholders = domains
+      .map((_, index) => `$${index + 1}`)
+      .join(",");
 
     // Create an array with domain names and their corresponding indices
     const indexedDomains = domains.map((domain, index) => ({
@@ -42,15 +44,12 @@ export default async function handler(
 
     // Execute the SELECT query with the given domains and order by the original indices
     const result: QueryResult<Domain> = await client.query(
-      `
-      SELECT d."domain-name", COALESCE(b."is-whitelisted", false) as "is-whitelisted"
+      `SELECT d."domain-name", COALESCE(b."is-whitelisted", false) as "is-whitelisted"
       FROM (
-        SELECT UNNEST(ARRAY[${domainPlaceholders}]::text[]) WITH ORDINALITY AS d("domain-name", index)
+        SELECT UNNEST(ARRAY[${domainPlaceholders}]::text[]) AS "domain-name", index
       ) AS d
       LEFT JOIN domains AS b ON d."domain-name" = b."domain-name"
-      ORDER BY d.index;
-      
-      `,
+      ORDER BY d.index;`,
       indexedDomains.map((domain) => domain.name)
     );
 
