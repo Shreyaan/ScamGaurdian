c5276a8a23bbc55c775ff06b3e422ff1597b2d97
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-24 00:17:27 +0530

    user story 1 design

---
+++ extension/src/DOM_Functions/generateModalHtml.js
@@ -0,0 +1,46 @@
+export function generateModalHTML(hostname) {
+  return `
+  <div class="scamgaurdian-blocking">
+  <div class="scamgaurdian-blocking-modal">
+    <div class="">
+      <p class="scamgaudian-heading">Scam Guardian</p>
+      <div class="scamgaudian-content">
+        <div class="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="67"
+            height="67"
+            viewBox="0 0 67 67"
+            fill="none"
+          >
+            <path
+              d="M2.79163 58.625H64.2083L33.5 5.58337L2.79163 58.625ZM36.2916 50.25H30.7083V44.6667H36.2916V50.25ZM36.2916 39.0834H30.7083V27.9167H36.2916V39.0834Z"
+              fill="#DE000B"
+            />
+          </svg>
+          <p>Warning</p>
+        </div>
+        <p>
+          We have detected that the following site is not safe. We advise
+          that you leave this website to avoid your data from being
+          compromised.
+        </p>
+        <p class="scamgaudian-link">
+          <a href="https://extension-orpin.vercel.app/">Learn more</a>
+        </p>
+        <p>${hostname}</p>
+        <div class="scamgaudian_bottom_btns">
+          <div class="scamgaudian-btn_stay" id="scamgaurdian_stay_btn">Stay</div>
+          <div
+          class="scamgaudian-btn-Leave"
+          onclick="window.location.href = "https://extension-orpin.vercel.app";"
+        >
+          <span>Leave website</span>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+}
--- extension/src/addButtonToWebsites.js
+++ extension/src/addButtonToWebsites.js
@@ -3,35 +3,13 @@ import { getVendorInfo } from "./helper/getVendorInfo.js";
 import { createButton, createPopup } from "./DOM_Functions/createElements.js";
 import { appendElementsToBody } from "./helper/appendElementsToBody.js";
 import { setButtonBehaviour } from "./DOM_Functions/setButtonBehaviour.js";
-import { getWebsiteBlacklsitStatus } from "./helper/getWebsiteStatus.js";
-import Toastify from "toastify-js";
+
 
 export let button = createButton();
 export let popup = createPopup();
 
 export async function addButtonToWebsites() {
   let currentUrl = window.location.href;
-
-  let hostname = new URL(currentUrl).hostname;
-
-  let blacklistStatus = await getWebsiteBlacklsitStatus(hostname);
-  if (blacklistStatus) {
-    Toastify({
-      text: "The following site might not be safe. We advise that you leave this website to avoid your data from being compromised. Learn more",
-      duration: 10000,
-      destination: "https://extension-orpin.vercel.app/",
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "right", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #ff3b3f, #ff757a)",
-        width: "25%",
-      },
-      onClick: function () {}, // Callback after click
-    }).showToast();
-  }
 
   let vendorData = await getVendorInfo(currentUrl);
   if (vendorData) {
