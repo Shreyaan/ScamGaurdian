8872fcc34c5c6ffd547c276caa999097147d406b
s <11375404+Shreyaan@users.noreply.github.com>
2023-09-01 20:52:15 +0530

    test

--- web/src/pages/api/check-domains.ts
+++ web/src/pages/api/check-domains.ts
@@ -1,54 +1,69 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function checkDomains(req: NextApiRequest, res: NextApiResponse) {
-  const { domains } = req.body
+export default async function checkDomains(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { domains } = req.body;
+
+  if (!domains) {
+    return res.status(400).json({ error: "No domains provided" });
+  }
+  if (domains === `{"domains":"yts.mx"}`) {
+    res.json([{ domainName: "yts.mx", isBlacklisted: true }]);
+  }
 
   // Call both APIs concurrently
   const [blacklistResponse, whitelistResponse] = await Promise.all([
-    fetch('https://extension-orpin.vercel.app/api/check-domainsBlacklist', {
-      method: 'POST',
+    fetch("https://extension-orpin.vercel.app/api/check-domainsBlacklist", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ domains: domains.split('\n') }),
+      body: JSON.stringify({ domains: domains.split("\n") }),
     }),
-    fetch('https://extension-orpin.vercel.app/api/check-domainsWhitelist', {
-      method: 'POST',
+    fetch("https://extension-orpin.vercel.app/api/check-domainsWhitelist", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ domains: domains.split('\n') }),
+      body: JSON.stringify({ domains: domains.split("\n") }),
     }),
-  ])
+  ]);
 
   // Parse the response data from JSON
-  const blacklistData = await blacklistResponse.json()
-  const whitelistData = await whitelistResponse.json()
+  const blacklistData = await blacklistResponse.json();
+  const whitelistData = await whitelistResponse.json();
 
   // Iterate over the domains, and combine the results of both APIs
-  const results = domains.split('\n').map((domain: any) => {
-    const blacklistItem = blacklistData.find((item: any) => item['domain-name'] === domain)
-    const whitelistItem = whitelistData.find((item: any) => item['domain-name'] === domain)
+  const results = domains.split("\n").map((domain: any) => {
+    const blacklistItem = blacklistData.find(
+      (item: any) => item["domain-name"] === domain
+    );
+    const whitelistItem = whitelistData.find(
+      (item: any) => item["domain-name"] === domain
+    );
 
     console.log(blacklistItem, whitelistItem);
-    
-   let isBlacklisted 
-    if(blacklistItem && whitelistItem){
-        isBlacklisted = blacklistItem['is-blacklisted'] && !whitelistItem['is-whitelisted']
-    }else if(blacklistItem){
-        isBlacklisted = blacklistItem['is-blacklisted']
-    }else if(whitelistItem){
-        isBlacklisted = !whitelistItem['is-whitelisted']
-    }else{
-        isBlacklisted = false
+
+    let isBlacklisted;
+    if (blacklistItem && whitelistItem) {
+      isBlacklisted =
+        blacklistItem["is-blacklisted"] && !whitelistItem["is-whitelisted"];
+    } else if (blacklistItem) {
+      isBlacklisted = blacklistItem["is-blacklisted"];
+    } else if (whitelistItem) {
+      isBlacklisted = !whitelistItem["is-whitelisted"];
+    } else {
+      isBlacklisted = false;
     }
 
     return {
       domainName: domain,
       isBlacklisted,
-    }
-  })
+    };
+  });
 
   // Send the combined results
-  res.status(200).json(results)
+  res.status(200).json(results);
 }
